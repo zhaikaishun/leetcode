f90889bf587642360b5bc66a991e2c7fd8ec8ccb
zhaikaishun <791808108@qq.com>
2018-02-14 16:43:15 +0800

    跳台阶和变态跳台阶，就是找规律而已，可以合理利用递归等方法

---
+++ src/剑指offer/变态跳台阶/Solution.java
@@ -0,0 +1,18 @@
+package 剑指offer.变态跳台阶;
+
+/**
+ * 一只青蛙一次可以跳上1级台阶，也可以跳上2级……它也可以跳上n级。
+ * 求该青蛙跳上一个n级的台阶总共有多少种跳法。
+ *
+ * 这种题目就是找规律
+ */
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.JumpFloorII(4));
+    }
+
+    public int JumpFloorII(int target) {
+        return 1<<(target-1);
+    }
+}
