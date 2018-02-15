4815a4628422819e330d6b258cb5277ed2fc3e96
zhaikaishun <791808108@qq.com>
2018-02-15 11:20:20 +0800

    写上题目

--- src/剑指offer/矩形覆盖/Solution.java
+++ src/剑指offer/矩形覆盖/Solution.java
@@ -3,7 +3,8 @@ package 剑指offer.矩形覆盖;
 import com.sun.org.apache.regexp.internal.RE;
 
 /**
- * Created by Administrator on 2018/2/15.
+ * 我们可以用2*1的小矩形横着或者竖着去覆盖更大的矩形。
+ * 请问用n个2*1的小矩形无重叠地覆盖一个2*n的大矩形，总共有多少种方法？
  */
 public class Solution {
     public static void main(String[] args) {
